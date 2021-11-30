ae4aa6786174dfbcaee1dd78695ae33b136cd6d6
Krishnakanta Maity <iamkkmcmd@gmail.com>
2021-12-01 03:06:11 +0530

    Add files via upload
    Upload files

---
+++ r_files/data_clean_process.R
@@ -0,0 +1,106 @@
+# Cleaning R-studio
+rm(list = ls())
+graphics.off()
+
+# Create main data file ------------------------------------------------------------------
+
+# Set the working directory
+setwd("C:/Users/iamkkmcmd/Desktop/PDS Project/data/")
+
+# Give short name to the column name
+col_names <- c('ts','age','sex','work','phy_ff','phy_health','phy_bw', 'phy_ex', 'meal',
+              'height','weight','exercise','fruit','veg','cook','spend','income','gymtime',
+              'disease','review','rate')
+
+# Read and Merge two data file
+data1 <- read.csv("DEF survey (Responses) - Form Responses 1.csv", 
+                  col.names = col_names)
+data2 <- read.csv("DEF2 (Responses) - Form Responses 1.csv",
+                  col.names = col_names)
+def <- rbind(data1[,], data2[,])
+rm(data1, data2)
+
+# See at a glance 
+def %>% glimpse()
+
+# Change variable type
+def$ts <- as.POSIXct(strptime(def$ts, format = "%m/%d/%Y %H:%M:%S"))
+def$age <- as.integer(def$age)
+def$meal <- factor(def$meal, labels = c("1","3","4","5","5+","2"))
+
+# exercise --> multilabel to list of labels
+# disease --> check the spelling for 'NO', 'no', 'No'....
+# review --> change to text data, where text mining is possible
+
+write.csv(def, file = "def_data.csv")
+
+
+# Scrutiny of data -----------------------------------------------------------------------
+
+def <- read.csv("def_data.csv")
+def <- def[,-1]
+
+# Summary of the data
+summary(def)
+
+# Comments from summary of data
+# Minimum age is 0.00, which is wrong data
+# There are one transgender respondent
+# Maximum height is 7777
+# Maximum weight is 9.998001e+14
+# Maximum spending is 687688678
+# Minimum income is 0
+
+# Let's look at data contain age is 0.00
+def[def$age == 0,]
+
+# Remove the absurd response from the data
+def <- def[-which(def$age == 0),]
+
+# Let's look at summary of the data
+summary(def)
+
+# Again we have maximum height is 182. Which is not in feet scale. So, need to change in 
+# particular scale. 1 feet = 30.48 cm
+def$height <- with(def, ifelse(height < 10, height*30.48, height))
+# Maximum value is 500kg, which is again outlier
+def <- def[-which(def$weight == 500),]
+def <- def[-which(def$weight < 30),]
+
+# Clean data
+write.csv(def, 'clean_data.csv')
+
+# Data Descriptions ----------------------------------------------------------------------
+
+# Variable Name				Description				                    Data Type
+# ________________________________________________________________________________________
+# ts 	    - Time Stamp							                    DateTime
+# age 		- Age of the respondent					                    Continuous	
+# sex 		- Gender of the respondent					                Factor
+# work		- Work preference of the respondent			                Factor
+# 
+# Ratings on Physical Condition: (in 10 scale)
+# phy_ff 	- Rating of liking fast food 			                	Factor
+# phy_health- Rating of healthiness					                    Factor
+# phy_bw 	- Rating of one's preference to maintain body weight		Factor	
+# phy_ex 	- Rating of importance of exercise			            	Factor
+# 
+# meal 		- Number of meals in a day				                	Discrete
+# height	- Height of the respondent		    		            	Continuous
+# weight 	- Weight of the respondent		    	                	Continuous
+# exercise 	- Type of exercises 						                Factor
+# 
+# Diet Culture: (Number of times)
+# fruit 	- Number of meals contain fruits				            Factor
+# veg		- Number of meals contain vegetables			        	Factor
+# cook		- Number of cooked plates				                	Factor
+# 
+# spend 	- Expenditure on fitness    				            	Continuous
+# income 	- Monthly family income					                    Continuous
+# gymtime 	- Time spent in gym. (In a day)				                Continuous
+# disease 	- Whether suffers from any regular disease		        	Categorical
+# 
+# review	- Review of the survey			    		            	Text
+# rate		- Rate the project topics and question		            	Factor
+
+
---
+++ r_files/plot.R
@@ -0,0 +1,227 @@
+# Cleaning R-studio
+rm(list = ls())
+graphics.off()
+
+# Load the libraries
+pacman::p_load(dplyr, ggplot2, tidyr, tidytext, lubridate, stringr)
+
+# Prilimary Plots ------------------------------------------------------------------------
+
+# Setting working directory and load the clean data file
+setwd("C:/Users/iamkkmcmd/Desktop/PDS Project/data/")
+source("C:/Users/iamkkmcmd/Desktop/PDS Project/r_files/functions.R")
+def <- read.csv("clean_data.csv")
+
+# Plot-1: Age distribution of respondents ------------------------------------------------
+def %>% 
+    ggplot(aes(x = sex, y = age, shape = sex, col = sex, size = 0.5)) +
+    geom_jitter() +
+    scale_shape_manual(values = c("Male" = "\u2642", "Female" = "\u2640")) +
+    geom_crossbar(data = aggregate(age~sex, def, mean), 
+                  aes(ymin = age, ymax = age),
+                  width = 0.5, size = 0.2) +
+    geom_text(data = def %>% group_by(sex) %>% summarise(avg_age = mean(age)),
+              aes(label = paste("Mean = ",round(avg_age,0)), y = 47)) +
+    geom_text(data = def %>% 
+                  group_by(sex) %>% 
+                  summarise(count = n()) %>% 
+                  mutate(percent = paste0(round(count/sum(count), 2),"%")),
+              aes(label = percent, y = 45)) +
+    labs(x = "Sex", y = "Age",
+         title = "Age Distribution of Respondents",
+         col = "Sex")  + custom_theme('none')
+
+# Some summary is used inside plot
+def %>% 
+    group_by(sex) %>% 
+    summarise(count = n()) %>% 
+    mutate(percent = paste0(round(count/sum(count), 2),"%"))
+def %>% group_by(sex) %>% summarise(avg_age = mean(age))
+
+# Plot-2: Scatterplot of height and weight ----------------------------------------
+def %>% 
+    ggplot(aes(height, weight, col = sex)) +
+    geom_point() + geom_rug() +
+    facet_wrap(.~sex) +
+    geom_point(data = def %>% 
+                   group_by(sex) %>% 
+                   summarise(avg_height = mean(height),
+                             avg_weight = mean(weight)),
+               aes(x = avg_height, y = avg_weight), size = 2, shape = 15, col = "black") +
+    geom_text(data = def %>% 
+                  group_by(sex) %>% 
+                  summarise(corr = cor(height, weight)),
+              aes(label = paste("Cor = ", round(corr,2)), x = 160, y = 100)) +
+    labs(x = "Height (in cm)", y = "Weight (in Kg)", col = "Gender",
+         title = "Scatter Plot of Height and Weight") +
+    custom_theme("top") 
+# Summary
+def %>% 
+    group_by(sex) %>% 
+    summarise(corr = cor(height, weight))
+
+
+# Plot-3: Gender-wise Work Preferences vs Weight ------------------------------------
+def %>% 
+    ggplot(aes(x = work, y = weight, fill = sex)) +
+    geom_violin() + geom_sina(alpha = 0.5) +
+    labs(x = "Work", y = "Weight", fill = "Sex",
+         title = "Gender-wise Work Preferences vs Weight") +
+    custom_theme()
+
+
+# Plot-4: Density Plot of Diet Habits -----------------------------------------------
+df_density <- def %>% 
+    select(fruit,veg,cook,spend) %>% 
+    mutate(id = 1:nrow(def)) %>% 
+    relocate(id, .before = "fruit") %>% 
+    gather(key, value, -c(id,spend))
+df_density %>% 
+    filter(spend < 15000) %>% 
+    ggplot(aes(x = spend)) +
+    geom_density(aes(fill = key), alpha = 0.4) +
+    facet_wrap(~value) + 
+    scale_x_discrete(limits = seq(0,10000,2500), labels = c("","2.5K","5K","7.5K","10K")) +
+    labs(x = "Amount of spending money", y = "Density",
+         title = "Density Plot of Diet Habits", fill = "Diet Habit") +
+    custom_theme()
+
+
+# Plot-5: Boxplot of Weights --------------------------------------------------------
+def %>% 
+    ggplot(aes(meal, weight)) +
+    geom_boxplot(color = "mediumpurple3", size = 0.8, alpha = 0.65, outlier.color = "red") +
+    labs(x = "Number of Meal", y = "Weight",
+         title = "Boxplot of Weights", subtitle = "(By number of meal)") +
+    custom_theme()
+
+
+# Plot-6: Histogram of Expenditure for Fitness --------------------------------------
+def %>% 
+    filter(spend < 5000) %>% 
+    ggplot(aes(x = spend)) + 
+    geom_histogram(aes(y = ..density..), binwidth = 250, fill = "slateblue") + 
+    geom_density(kernel = "gaussian", col = "tomato4", size = 1) +
+    labs(x = "Spend", y = "Frequency", 
+         title = "Histogram of Expenditure for Fitness") + 
+    custom_theme()
+
+
+# Plot-7.1: Donut Chart of Exercise Preferences ---------------------------------------
+def$exercise <- str_replace_all(def$exercise,"Do not interested","Not interested")
+exercises <- unique(trimws(unlist(strsplit(def$exercise, ','))))
+mat <- t(sapply(def$exercise, function(x){ifelse(exercises %in% x, 1, 0)}))
+rownames(mat) <- paste("Person", 1:nrow(def))
+colnames(mat) <- exercises
+df <- data.frame(exercises,freq = colSums(mat), row.names = NULL)
+df <- df %>% mutate(r_freq = freq/sum(freq),
+                    ymax = cumsum(r_freq),
+                    ymin = lag(ymax, default = 0),
+                    pos = (ymax + ymin)/2,
+                    label = paste0(exercises,'-', round(r_freq*100,0), '%'))
+df %>% 
+    ggplot(aes(ymin = ymin, ymax = ymax, xmin = 3, xmax = 4, fill = exercises)) +
+    geom_rect() + xlim(c(2,4)) +
+    coord_polar(theta = "y") +
+    geom_label(x = 3.5, aes(y = pos, label = label), size = 3.2) +
+    custom_theme("none") + 
+    annotate("text", x = 2, y = 0.5, label = "EXERCISE", size = 10) +
+    labs(x = " ", y = "", title = "Donut Chart of Exercise Preferences") +
+    theme(axis.text.x = element_blank(),
+          axis.ticks.y = element_blank(),
+          axis.text.y = element_blank(),
+          panel.grid = element_blank()) -> g
+# ggsave("../figure/fig7_donut_chart_of_exercise_preferences.png",g,"png",dpi = 1500, height = 8, width = 8)
+
+
+## Plot-7.2: Density of Walkers' Age -------------------------------------------------
+def %>% 
+    filter(exercise == "Walk") %>% 
+    ggplot(aes(age)) +
+    labs(x = "Age", y = "Density",
+         title = "Density of Walkers' Age") +
+    geom_density(fill = "#c21dbf", alpha = 0.5, size = 1,inherit.aes = T) +
+    custom_theme() 
+
+# Plot-8.1: Fondness of Fast-Food & Healthiness ---------------------------------------
+def %>% 
+    ggplot(aes(x = phy_ff, y = phy_health, col = sex)) +
+    geom_jitter() +
+    scale_x_discrete(labels = 1:10, limits = 1:10) +
+    scale_y_discrete(labels = 1:10, limits = 1:10) +
+    labs(x = "Rating of liking fast food",
+         y = "Rating of healthiness",
+         title = "Fondness of Fast-Food & Healthiness", col = "Sex") +
+    custom_theme()
+
+## Plot-8.2: Fondness of Fast-Food & Healthiness -------------------------------------
+def %>% 
+    filter(phy_ff <= 5 & phy_health <= 5) %>% 
+    ggplot(aes(x = phy_ff, y = phy_health, col = gymtime)) +
+    geom_jitter() +
+    scale_x_discrete(labels = 1:5, limits = 1:5) +
+    scale_y_discrete(labels = 1:5, limits = 1:5) +
+    labs(x = "Rating of liking fast food",
+         y = "Rating of healthiness",
+         title = "Fondness of Fast-Food & Healthiness", col = "Gym-time") +
+    custom_theme() -> g
+
+ggsave("../figure/fig8_2_clustering.png",g,"png",dpi = 1500)
+
+# Plot-9: Bar Chart of Ratings of this study ----------------------------------------
+def %>% 
+    group_by(rate, sex) %>% 
+    summarise(count = n()) %>% 
+    ggplot(aes(rate, count, fill = sex)) +
+    geom_bar(stat = 'identity', position = 'dodge') +
+    labs(x = "Ratings", y = 'Frequency', fill = "Sex",
+         title = "Bar Chart of Ratings of this study",
+         subtitle = "(Grouped by gender)") +
+    custom_theme()
+
+# Plot-10: Weight & Healthiness According to Gym-time -------------------------------
+def %>% 
+    filter(gymtime != "More than 3 hours") %>% 
+    ggplot(aes(x = factor(phy_health), y = weight, fill = gymtime)) +
+    geom_dotplot(binaxis = "y", stackdir = "center", position = "dodge", shape = 5, colour = NA) +
+    facet_wrap(~gymtime) + 
+    scale_x_discrete(labels = 1:10, limits = 1:10) +
+    labs(y = "Weight (in Kg.)",
+         x = "Rating of healthiness",
+         title = "Weight & Healthiness According to Gym-time", fill = "Gym-time") +
+    custom_theme() 
+
+
+
+# Plot-trying: Likart Scale plot ----------------------------------------------------
+likart <- def %>% 
+    select(starts_with('phy')) %>%
+    mutate(id = 1:nrow(def)) %>% 
+    relocate(id, .before = phy_ff) %>% 
+    pivot_longer(-id)
+likart %>% 
+    # filter(id %in% sample(1:nrow(def), 15, replace = FALSE)) %>% 
+    ggplot(aes(x = value, y = name, fill = name, group = id)) +
+    geom_line(aes(col = factor(id))) +
+    geom_point() + coord_flip()
+
+
+likart %>% glimpse()
+likart <- likart %>% filter(key == "phy_ff")
+likart1 <-  t(sapply(likart$value, function(x){ifelse(1:10 %in% x, x, 0)}))
+colnames(likart1) <- 1:10
+likart <- cbind(likart,likart1)
+
+likart %>%
+    select(-c("key")) %>% 
+    gather('id','value',2:11) %>% 
+    ggplot(aes(x = '',y = value)) + geom_line()
+
+sapply(def[,6:9], function(x){ifelse(x %in% 1:10, 1, 0)})
+
+sapply(starwars$films, function(x){ifelse(unique(unlist(starwars$films)) %in% x, 1, 0)})
+
+ggplot(def, aes(height, weight)) + geom_density2d()
+
+# ggsave("../figure/fig_7_2_age_distribution_of_walkers.png",p,"png",dpi = 1500)
+
